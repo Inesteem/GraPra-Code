cff17a4dfaa80124a605078849417c6d7c657311
Oliver Brehm <uq27enym@stud.informatik.uni-erlangen.de>
2015-01-14 18:11:45 +0100

    missing networking files

---
+++ a5-multiman/src/server-networking.cpp
@@ -0,0 +1,33 @@
+#include "server-networking.h"
+
+namespace client_connections {
+    unsigned int sockets = 0;
+    boost::asio::ip::tcp::socket **socket;
+    server_message_reader **reader;
+}
+
+wall_time_timer since_last_broadcast;
+
+void broadcast(msg::message *m)
+{
+    int i = 0;
+    try {
+        for (i = 0; i < client_connections::sockets; ++i)
+            boost::asio::write(*client_connections::socket[i], boost::asio::buffer(m, m->message_size), boost::asio::transfer_all());
+    }
+    catch (boost::system::system_error &err) {
+        cerr << "Error sending message to client of player " << i << ": " << err.what() << "\nExiting." << endl;
+        quit(-1);
+    }
+    since_last_broadcast.restart();
+}
+
+void quit(int status)
+{
+    throw quit_signal(status);
+}
+
+void server_message_reader::handle_message(msg::spawn_troup_client *m) {
+    cout << "Spawning troup..." << endl;
+    m_gameStage->spawnTroup(m->sourceId, m->destinationId, 5);
+}
---
+++ a5-multiman/src/server-networking.h
@@ -0,0 +1,41 @@
+#ifndef __SERVER_NETWORKING_H
+#define __SERVER_NETWORKING_H
+
+#include "messages.h"
+#include "wall-timer.h"
+#include "server-logic.h"
+
+using namespace std;
+
+void broadcast(msg::message *m);
+
+class GameStage;
+class server_message_reader : public message_reader {
+    int player_id; // the messages are sent by this player
+    GameStage *m_gameStage;
+
+public:
+    server_message_reader(GameStage *gameStage, message_reader::socket *socket, int player) : m_gameStage(gameStage), message_reader(), player_id(player) {
+        setSocket(socket);
+    }
+
+    void handle_message(msg::spawn_troup_client *m);
+};
+
+namespace client_connections {
+    extern unsigned int sockets;
+    extern boost::asio::ip::tcp::socket **socket;
+    extern server_message_reader **reader;
+}
+
+struct quit_signal {
+    int status;
+    quit_signal(int s) : status(s) {}
+};
+void quit(int status);
+
+extern wall_time_timer since_last_broadcast;
+
+
+void broadcast(msg::message *m);
+#endif
